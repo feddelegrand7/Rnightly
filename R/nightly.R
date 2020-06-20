0146e1697c5cf91ceeac77c422100fbe4610eb2c
feddelegrand7 <ihaddaden.fodeil@gmail.com>
2020-06-21 01:53:26 +0200

    documentation done

--- R/nightly.R
+++ R/nightly.R
@@ -1,4 +1,51 @@
+#' Implement a dark/light toggle mode by default
+#'
+#' @description Implement a default dark/light toggle mode that will be triggered by any shiny element. The user can also define other colors to work with.
+#' @param trigElement the id of the shiny element that will trigger the toggle mode
+#' @param bodyColor the color of the application's body after clicking on the trigElement. Defaults to #282828 (dark)
+#' @param txtColor the color of the text within the application after clicking on the trigElement. Defaults to #f5f5f5 (white)
+#' @param inpTxtColor the color of the text within the shiny inputs after clicking on the trigElement. Defaults to #f5f5f5 (white)
+#' @param inpBgColor the color of the background of the shiny inputs after clicking on the trigElement. Defaults to #313131 (Very Dark Grey)
+#'
+#' @return A default dark/light toggle mode in shiny ui
+#' @export
+#'
+#' @examples
+#' if (interactive()) {
 
+#' ui <- fluidPage(
+#'  use_nightly(),
+#'
+#'  h1("Click on the Plot to Toggle a Dark/Light Mode in you Shiny Application"),
+#'
+#'  plotOutput(outputId = "plt1"),
+#'
+#'  nightly(trigElement = "plt1")
+#'
+#' )
+#'
+#'
+#' server <- function(input, output) {
+#'
+#' output$plt1 <- renderPlot({
+#'
+#'
+#' plot(mtcars)
+#'
+#'
+#' })
+#'
+#'
+#' }
+#'
+#'
+#' shinyApp(ui = ui, server = server)
+#'
+#'
+#' }
+#'
+#'
+#'
 
 nightly <- function(trigElement, bodyColor = "#282828", txtColor = "#f5f5f5", inpTxtColor = "#f5f5f5", inpBgColor = "#313131"){
 
@@ -9,9 +56,9 @@ nightly <- function(trigElement, bodyColor = "#282828", txtColor = "#f5f5f5", in
 
 var nightModeConfig = {{
   body: '{bodyColor}',
-  texts: '{textColor}',
+  texts: '{txtColor}',
   inputs: {{
-    color: '{inpColor}',
+    color: '{inpTxtColor}',
     backgroundColor: '{inpBgColor}',
   }}
 }};
--- R/use_nightly.R
+++ R/use_nightly.R
@@ -1,5 +1,23 @@
 
 
+#' Enables the nightly javascript library
+#'
+#' @description The function activates the capabilities of the nightly javascript library. The user can put it anywhere within the UI.
+#'
+#' @return called for the side effect of activating the scrollrevealjs library
+#' @export
+#'
+#' @examples \donttest{
+#'
+#' # Put the function within your shiny ui
+#'
+#' use_nightly()
+#'
+#'
+#'
+#' }
+
+
 use_nightly <- function(){
 
 
